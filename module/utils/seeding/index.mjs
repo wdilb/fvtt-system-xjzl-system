a77f1756246ac6599e5c537a477da09c6b3effbd
Tiwelee <awdilb@126.com>
2026-01-01 21:20:01 +0800

    将消耗品的导入加入到了all中

--- module/utils/seeding/index.mjs
+++ module/utils/seeding/index.mjs
@@ -16,11 +16,12 @@ export const SeedingManager = {
     all: async function() {
         const confirm = await Dialog.confirm({
             title: "全量重置合集包",
-            content: "<p>这将清空并重新生成所有系统预设合集包（身世、性格等）。确定吗？</p>"
+            content: "<p>这将清空并重新生成所有系统预设合集包。确定吗？</p>"
         });
         
         if (confirm) {
             await this.origins();
+            await this.consumables();
             // await this.wuxue();
             ui.notifications.info("XJZL | 全量种子数据生成完成。");
         }
