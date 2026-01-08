ed679e89bf82f1ebd7fb7794982115d3b99b7424
Tiwelee <awdilb@126.com>
2026-01-08 17:37:39 +0800

    添加了江南阁的武学数据

--- module/utils/seeding/seed-wuxue.mjs
+++ module/utils/seeding/seed-wuxue.mjs
@@ -17,7 +17,7 @@ const SECT_MAP = {
     "gaibang": "丐帮",
     "fenghuayuan": "枫华院",
     "liushanmen": "六扇门",
-    // "jiangnange": "江南阁",
+    "jiangnange": "江南阁",
     // "shenfengbang": "神风帮",
     // "sihaibiaomeng": "四海镖盟",
     // "jiangjunying": "将军营",
